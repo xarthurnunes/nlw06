8db910338be65be618d1cd04d3cf41416a4ce357
Arthur Nunes Catarina <arthur.nunes@hydrasystems.com.br>
2021-06-22 20:32:56 -0300

    Segundo dia NLW06

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,16 @@
+import "reflect-metadata";
 import express from "express";
+import {router} from "./routes";
+
+
+import "./database"
+
 
 const app = express();
 
-/**
- * GET     => Buscar uma informação
- * POST    => Inserir(Criar) uma informação
- * PUT     => Alterar uma informação
- * DELETE  => Remover um dado
- * PATCH   => Alterar uma informação específica
- */
+app.use(express.json());
 
-
-app.get("/test", (request, response) => {
-    //Request => Entrando
-    //Response => Saindo
-    return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW método POST");
-});
+app.use(router);
 
 // http://localhost:3000
-app.listen(3000, () => console.log("Server is Running NLW"));
+app.listen(3000, () => console.log("Server is Running"));
